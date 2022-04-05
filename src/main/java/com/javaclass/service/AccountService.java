b868015060e2938280c248e3421cac5455a6ee59
zzeennee <96711823+zzeennee@users.noreply.github.com>
2022-04-05 12:31:52 +0900

    0405
    프로젝트 변경

--- src/main/java/com/javaclass/service/AccountService.java
+++ src/main/java/com/javaclass/service/AccountService.java
@@ -1,10 +1,18 @@
 package com.javaclass.service;
 
+import java.util.List;
+
 import com.javaclass.domain.AccountVO;
 
 public interface AccountService {
 
 	public void insertAccount(AccountVO vo);
 	
-	//public AccountVO idCheck_Login(AccountVO vo);
+	public AccountVO loginCheck(AccountVO vo);
+	
+	public void updateAccount(AccountVO vo);
+	
+	public AccountVO myHomePageView(AccountVO vo);
+	
+	public List<AccountVO> accountList(AccountVO vo);
 }
