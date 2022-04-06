6fcd2e26cefa50d3baba5d42014ef180005b99f8
2HaeYun <ducksoo311@naver.com>
2022-04-06 22:56:07 +0900

    0406_Reply
    댓글 작성 기능 수정
    작성한 댓글 목록에 보이기

--- src/main/java/com/javaclass/dao/ReplyDAO.java
+++ src/main/java/com/javaclass/dao/ReplyDAO.java
@@ -11,5 +11,14 @@ public interface ReplyDAO {
 	
 	//댓글 작성
 	public void writdReply(ReplyVO vo) throws Exception;
+	
+	//댓글 수정
+	public void updateReply(ReplyVO vo)throws Exception;
+	
+	//댓글 삭제
+	public void deleteReply(ReplyVO vo)throws Exception;
+	
+	//선택된 댓글 조회
+	public ReplyVO selectReply(int rno)throws Exception;
 
 }
